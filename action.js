76a35dff09aa0d42dfbb8592781797b7f5e5808d
rylero <ryan@leroy.com>
2019-12-08 12:40:40 -0800

    Add files via upload
    my initial verion

---
+++ action.js
@@ -0,0 +1,46 @@
+var cartList = document.getElementById('cartList');
+var totalCost = 0;
+var total = document.getElementById('total');
+var address;
+var emailAddress;
+
+var ch;
+var pb;
+var nm;
+var tc;
+var sn;
+var cform = document.forms[0];
+
+var time;
+
+function addToCart(name) {
+	var node = document.createElement("LI");
+	node.appendChild(document.createTextNode(name));
+	cartList.appendChild(node);     
+	totalCost+=6.50;
+	total.innerHTML = "$" + totalCost;
+	if (name.startsWith("Chocolate Chip")) {
+		cform.elements["ChocolateChip"].value++;
+	} else if (name.startsWith("Peanut Butter")) {
+		cform.elements["PeanutButter"].value++;
+	} else if (name.startsWith("Neiman Marcus")) {
+		cform.elements["NeimanMarcus"].value++;
+	} else if (name.startsWith("Tiger Cookies")) {
+		cform.elements["TigerCookies"].value++;
+	} else if (name.startsWith("Snickerdoodle")) {
+		cform.elements["Snickerdoodle"].value++;
+	}
+}
+
+function clearCart() {
+	document.getElementById("cartList").innerHTML = "";
+	cform.elements["ChocolateChip"].value = 0;
+	cform.elements["PeanutButter"].value = 0;
+	cform.elements["NeimanMarcus"].value = 0;
+	cform.elements["TigerCookies"].value = 0;
+	cform.elements["Snickerdoodle"].value = 0;
+}
+
+function purchase(){
+	alert("Thank you for ordering.\nI will collect the $"+totalCost+" when I deliver the cookies.");
+}
